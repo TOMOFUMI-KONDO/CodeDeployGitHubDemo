66e7df76c4618f0246063767b8370729b09e6507
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-04 22:59:14 +0900

    create app

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from "express"
+
+const app = express()
+
+app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next()
+})
+
+const port = 3000
+app.listen(port, () => console.log("Listening on http://localhost:", port))
+
+app.use((req, res, next) => {
+    console.log("Path: ", req.originalUrl)
+    console.log("Headers:", req.headers)
+    console.log("Body:", req.body)
+    next()
+})
+
+app.get("/", (req, res) => res.send("blue"))
+
+app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+    console.error(err)
+    res.status(500).send({ error: err.toString() })
+})
