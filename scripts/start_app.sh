e1c5b7db9a66fd3bec7bbbed395031eceef8f551
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-05 22:45:33 +0900

    fixed start up script

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /app && node . > /var/log/app/info.log 2> /var/log/app/error.log &
+cd /app && NODE_ENV=production node . > /var/log/app/info.log 2> /var/log/app/error.log < /dev/null &
