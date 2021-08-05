79e7dcd1a44e8897f66f65a88a95dc979d3ca863
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-06 00:13:24 +0900

    fix

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /app && NODE_ENV=production yarn start > /var/log/app/info.log 2> /var/log/app/error.log&
+cd /app && NODE_ENV=production yarn start > /var/log/app/info.log 2> /var/log/app/error.log &
