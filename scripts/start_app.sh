b1818efd671fc113dd1694eec6fc6d8911780ea2
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-05 23:09:23 +0900

    fix scripts

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /app && NODE_ENV=production node . > /var/log/app/info.log 2> /var/log/app/error.log < /dev/null &
+cd /app && NODE_ENV=production yarn start > /var/log/app/info.log 2> /var/log/app/error.log &
--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-PID=$(ps -C node -o pid=)
+PID=$(pgrep -f "/usr/bin/node .")
 if [[ $PID != "" ]] ; then kill -9 "$PID" ; fi
