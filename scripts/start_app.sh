ef83c4b8be621b10dd438747296aab09646dc210
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-05 23:33:34 +0900

    update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export NODE_ENV=production
-cd /app && yarn start > /var/log/app/info.log 2> /var/log/app/error.log &
+cd /app && yarn start > /var/log/app/info.log 2> /var/log/app/error.log < /dev/null &
