fe9eaf42593372aacb0f294d54a70731e0fbf5d6
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-05 19:00:45 +0900

    specify port by env

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
     next()
 })
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log("Listening on http://localhost:", port))
 
 app.use((req, res, next) => {
